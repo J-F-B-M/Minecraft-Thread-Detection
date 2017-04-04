0aa5332d9c1329db3095e1d99ddde524e544e814
J-F-B-M <joachim.brehmer@gmail.com>
2017-04-05 00:11:52 +0200

    Multilabel-Regression Test

---
+++ neural-network/src/main/java/BigSmallEvenOddClassifier.java
@@ -0,0 +1,95 @@
+import org.deeplearning4j.berkeley.Pair;
+import org.deeplearning4j.datasets.iterator.DoublesDataSetIterator;
+import org.deeplearning4j.eval.RegressionEvaluation;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.GravesLSTM;
+import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.NormalizerMinMaxScaler;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Created by Joachim on 04.04.2017.
+ */
+public class BigSmallEvenOddClassifier {
+    private static final Logger LOGGER = Logger.getLogger(BigSmallEvenOddClassifier.class.getName());
+
+    public static void main(String[] args) {
+
+        int trainSize = 1000;
+
+        DataSetIterator trainDataIter = new DoublesDataSetIterator(IntStream.iterate(0, i -> i + 3).limit(trainSize).mapToObj(i -> {
+            return new Pair<>(new double[]{i}, new double[]{i < trainSize * 3 / 2.0 ? 0 : 1, i % 2 == 0 ? 1 : 0});
+        }).collect(Collectors.toList()), 1);
+
+        DataSetIterator testDataIter = new DoublesDataSetIterator(IntStream.iterate(1, i -> i + 3).limit(trainSize).mapToObj(i -> {
+            return new Pair<>(new double[]{i}, new double[]{i < trainSize * 3 / 2.0 ? 0 : 1, i % 2 == 0 ? 1 : 0});
+        }).collect(Collectors.toList()), 1);
+
+        //Normalize the training data
+        NormalizerMinMaxScaler normalizer = new NormalizerMinMaxScaler(0, 1);
+        normalizer.fitLabel(true);
+        normalizer.fit(trainDataIter);              //Collect training data statistics
+        trainDataIter.reset();
+
+        trainDataIter.setPreProcessor(normalizer);
+        testDataIter.setPreProcessor(normalizer);
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(140)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .iterations(1)
+                .weightInit(WeightInit.XAVIER)
+                .updater(Updater.NESTEROVS).momentum(0.9)
+                .learningRate(0.15)
+                .list()
+                .layer(0, new GravesLSTM.Builder().activation(Activation.TANH).nIn(1).nOut(10)
+                        .build())
+                .layer(1, new RnnOutputLayer.Builder(LossFunctions.LossFunction.MSE)
+                        .activation(Activation.IDENTITY).nIn(10).nOut(2).build())
+                .build();
+
+        MultiLayerNetwork net = new MultiLayerNetwork(conf);
+        net.init();
+
+        net.setListeners(new ScoreIterationListener(20));
+
+        // ----- Train the network, evaluating the test set performance at each epoch -----
+        int nEpochs = 50;
+
+        for (int i = 0; i < nEpochs; i++) {
+            net.fit(trainDataIter);
+            trainDataIter.reset();
+            LOGGER.info("Epoch " + i + " complete. Time series evaluation:");
+
+            RegressionEvaluation evaluation = new RegressionEvaluation(2);
+
+            //Run evaluation. This is on 25k reviews, so can take some time
+            while (testDataIter.hasNext()) {
+                DataSet t = testDataIter.next();
+                INDArray features = t.getFeatureMatrix();
+                INDArray labels = t.getLabels();
+                INDArray predicted = net.output(features, true);
+
+                evaluation.evalTimeSeries(labels, predicted);
+            }
+
+            System.out.println(evaluation.stats());
+
+            testDataIter.reset();
+        }
+    }
+}
